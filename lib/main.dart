71ea0759d151ebb13c3e65b4be3647a8929423b0
Michael <90962435+Michael679089@users.noreply.github.com>
2025-06-28 16:52:09 +0800

    made new branch for login test

--- lib/main.dart
+++ lib/main.dart
@@ -69,6 +69,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
     //
@@ -103,13 +104,14 @@ class _MyHomePageState extends State<MyHomePage> {
           // action in the IDE, or press "p" in the console), to see the
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          children:
+            <Widget>[
+              const Text('Edit: You have pushed the button this many times:'),
+              Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+            ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
